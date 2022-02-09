54d58a7289c25efa9566bd00f1bd68283e22ea1d
Daniel Degirmen <daniel.degirmen@sgu.se>
2022-02-09 16:11:34 +0100

    Added ability to add or update a visit, closes #57

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/VisitControllerTest.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// TODO: WRITE TESTS
+
+class VisitControllerTest {
+
+    @Test
+    void initNewVisitForm() {
+    }
+
+    @Test
+    void addNewvisit() {
+    }
+}
