464e1d0f483f3c362709bcd1129c4ac7f35a2634
Daniel Degirmen <daniel.degirmen@sgu.se>
2022-02-09 12:31:25 +0100

    Implemented Find Owner, closes #54

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,18 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @RequestMapping("/owners")
 @Controller
@@ -25,15 +29,41 @@ public class OwnerController {
         dataBinder.setDisallowedFields("id"); // don't allow web form to send an id to be bound to object
     }
 
-    @GetMapping({"", "/index", "/index.html"})
-    public String listOwners(Model model) {
-        model.addAttribute("owners", ownerService.findAll());
-        return "owners/index";
-    }
+//    @GetMapping({"", "/index", "/index.html"})
+//    public String listOwners(Model model) {
+//        model.addAttribute("owners", ownerService.findAll());
+//        return "owners/index";
+//    }
 
     @GetMapping("/find")
-    public String findOwners() {
-        return "notimplemented";
+    public String findOwners(Model model) {
+        model.addAttribute("owner", Owner.builder().build());
+        return "owners/findOwners";
+    }
+
+
+    @GetMapping
+    public String processFindForm(Owner owner, BindingResult result, Model model) {
+        if (owner.getLastName() == null) {
+            owner.setLastName("");
+        }
+
+        String lastName = owner.getLastName();
+        List<Owner> ownersResults = ownerService.findAllByLastNameLike(lastName);
+        if (ownersResults.isEmpty()) {
+            // no owners found
+            result.rejectValue("lastName", "notFound", "not found");
+            return "owners/findOwners";
+        }
+        else if (ownersResults.size() == 1) {
+            // 1 owner found
+            owner = ownersResults.iterator().next();
+            return "redirect:/owners/" + owner.getId();
+        }
+        else {
+            model.addAttribute("selections", ownersResults);
+            return "owners/ownersList";
+        }
     }
 
     @GetMapping("/{ownerId}")
