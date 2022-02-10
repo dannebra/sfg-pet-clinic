ec6a113fe39e7ccfc8ed8aa760db5b9649a945cf
Daniel Degirmen <daniel.degirmen@sgu.se>
2022-02-10 16:08:27 +0100

    Wrote tests for Visit Controller, closes #63

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/VisitControllerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/VisitControllerTest.java
@@ -1,18 +1,63 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.VisitService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-// TODO: WRITE TESTS
-
+@ExtendWith(MockitoExtension.class)
 class VisitControllerTest {
 
-    @Test
-    void initNewVisitForm() {
+    @Mock
+    VisitService visitService;
+
+    @Mock
+    PetService petService;
+
+    @InjectMocks
+    VisitController visitController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp() {
+        Pet pet = new Pet();
+        when(petService.findById(anyLong())).thenReturn(pet);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(visitController).build();
     }
 
     @Test
-    void addNewvisit() {
+    void initNewVisitForm() throws Exception {
+        mockMvc.perform(get("/owners/1/pets/1/visits/new"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("visit"))
+                .andExpect(model().attributeExists("pet"))
+                .andExpect(view().name("pets/createOrUpdateVisitForm"));
+    }
+
+    @Test
+    void addNewvisit() throws Exception {
+        mockMvc.perform(post("/owners/1/pets/2/visits/new"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(model().attributeExists("visit"))
+                .andExpect(view().name("redirect:/owners/1"));
+
+        verify(visitService).save(any());
     }
 }
